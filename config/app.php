17bbbb0d468d9e4fdaa52ecef97369aa15d27e2b
slikerareheight <71325274+slikerareheight@users.noreply.github.com>
2026-09-24 18:02:17 +0100

    Harden Eleven8 session cookies

--- config/app.php
+++ config/app.php
@@ -1,5 +1,14 @@
 <?php
 declare(strict_types=1);
+
+$secure=!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS']!=='off';
+session_set_cookie_params([
+    'lifetime'=>0,
+    'path'=>'/',
+    'secure'=>$secure,
+    'httponly'=>true,
+    'samesite'=>'Lax'
+]);
 session_name('eleven8_session');
 session_start();
 define('ELEVEN8_ROOT', dirname(__DIR__));
